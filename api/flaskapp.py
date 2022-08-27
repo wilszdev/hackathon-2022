2d29e3f954d019e6a38ec2df6e6095fac6461e8c
wilszdev <wilszdev@gmail.com>
2022-08-27 17:46:50 +1000

    integrate google cloud ai model for drawing evaluation

--- api/flaskapp.py
+++ api/flaskapp.py
@@ -1,10 +1,20 @@
 from random import choice
-from click import prompt
 from flask import Flask, abort, jsonify, request
 from time import time_ns
 from uuid import uuid4
 import os
 from flask_cors import CORS
+import json
+from sus import SusClient
+
+
+with open('.\\api\\config.json', 'r') as f:
+    config = json.loads(f.read())
+
+
+sus_client = SusClient(
+    project=config['project'],
+    endpoint_id=config['endpoint_id'])
 
 
 app = Flask(__name__)
@@ -49,12 +59,23 @@ def make_submission_for_prompt(prompt_id):
 
     data = request.get_data()
 
-    with open(os.path.join(dst_folder, f"{prompt_id}.png"), 'bw+') as f:
+    filepath = os.path.abspath(os.path.join(dst_folder, f"{prompt_id}.png"))
+
+    with open(filepath, 'bw+') as f:
         f.write(data)
+
+    result_dict = sus_client.predict(filepath)
+
+    prompt = prompts[prompt_id]['prompt']
+
+    score = 0
+    if prompt in result_dict:
+        confidence = result_dict[prompt]
+        score = int(confidence * 100)
 
     response = {
         'prompt_id': prompt_id,
-        'score': 69
+        'score': score
     }
 
     return jsonify(response), 200
---
+++ api/sus.py
@@ -0,0 +1,69 @@
+import os
+import base64
+from google.cloud import aiplatform
+from google.cloud.aiplatform.gapic.schema import predict
+
+
+credential_path = os.path.abspath(".\\api\\keyfile.json")
+print(f"{credential_path = }")
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
+
+class SusClient:
+    def __init__(
+        self,
+        project: str,
+        endpoint_id: str,
+        location: str = "us-central1",
+        api_endpoint: str = "us-central1-aiplatform.googleapis.com"
+    ):
+        self.project = project
+        self.location = location
+        self.endpoint_id = endpoint_id
+        self.client_options = {"api_endpoint": api_endpoint}
+        self.client = aiplatform.gapic.PredictionServiceClient(
+            client_options=self.client_options)
+
+    def predict(self, filepath: str):
+        with open(filepath, "rb") as f:
+            file_content = f.read()
+        encoded_content = base64.b64encode(file_content).decode('utf-8')
+        instance = predict.instance.ImageClassificationPredictionInstance(
+            content=encoded_content
+        ).to_value()
+
+        instances = [instance]
+
+        parameters = predict.params.ImageClassificationPredictionParams(
+            confidence_threshold=0.25, max_predictions=5
+        ).to_value()
+
+        endpoint = self.client.endpoint_path(
+            project=self.project,
+            location=self.location,
+            endpoint=self.endpoint_id
+        )
+
+        response = self.client.predict(
+            endpoint=endpoint, instances=instances, parameters=parameters
+        )
+
+        # dict looks like this:
+        #
+        #   {
+        #       'confidences': [0.5, 0.3, 0.2],
+        #       'displayNames': [],
+        #       'ids': []
+        #   }
+
+        assert len(response.predictions) == 1
+
+        prediction = dict(next(iter(response.predictions)))
+
+        payload = {}
+        for i in range(len(prediction['displayNames'])):
+            name = prediction['displayNames'][i]
+            confidence = prediction['confidences'][i]
+            payload.update({name: confidence})
+
+        return payload
