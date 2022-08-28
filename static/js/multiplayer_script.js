a8a4205754dd1894b4704785f584adb99ca47bfd
wilszdev <wilszdev@gmail.com>
2022-08-28 12:23:18 +1000

    update gallery script

--- static/js/multiplayer_script.js
+++ static/js/multiplayer_script.js
@@ -1,11 +1,21 @@
 function displayAll() {
+  for (let i = 0; i < 6; ++i) {
+    let n = i + 1;
+    $(`#image${n}`).hide()
+    $(`#prompt${n}`).hide()
+    $(`#score${n}`).hide()
+  }
+  
   api_get_recent((results)=>{
     for (let i = 0; i < results.length; ++i) {
       res = results[i]
       let n = i + 1;
       $(`#image${n}`).attr('src', `data:image/png;base64,${res.img}`)
-      $(`#prompt${n}`).text(res.desc)
+      $(`#prompt${n}`).text(res.description)
       $(`#score${n}`).text(res.score)
+      $(`#image${n}`).show()
+      $(`#prompt${n}`).show()
+      $(`#score${n}`).show()
     }
   });
 };
