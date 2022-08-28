fe52a5b2def61cc59a30d75f3d4492f9d180f4d8
wilszdev <wilszdev@gmail.com>
2022-08-28 13:49:43 +1000

    final updates to gallery

--- static/js/multiplayer_script.js
+++ static/js/multiplayer_script.js
@@ -1,25 +1,15 @@
-function displayAll() {
-  for (let i = 0; i < 6; ++i) {
+api_get_recent((results)=>{
+  for (let i = 0; i < results.length && i < 6; ++i) {
+    res = results[i]
     let n = i + 1;
-    $(`#image${n}`).hide()
-    $(`#prompt${n}`).hide()
-    $(`#score${n}`).hide()
+    $(`#image${n}`).attr('src', `data:image/png;base64,${res.img}`);
+    $(`#prompt${n}`).text(res.description);
+    $(`#score${n}`).text(res.score);
+    $(`#element${n}`).show();
   }
-  
-  api_get_recent((results)=>{
-    for (let i = 0; i < results.length; ++i) {
-      res = results[i]
-      let n = i + 1;
-      $(`#image${n}`).attr('src', `data:image/png;base64,${res.img}`)
-      $(`#prompt${n}`).text(res.description)
-      $(`#score${n}`).text(res.score)
-      $(`#image${n}`).show()
-      $(`#prompt${n}`).show()
-      $(`#score${n}`).show()
-    }
-  });
-};
-
-interval = setInterval(() => {
-  displayAll();
-}, 5000);
+  for (let i = results.length; i < 6; ++i)
+  {
+    let n = i + 1;
+    $(`#element${n}`).hide();
+  }
+});
