da0a458f0c1051df1ceb94123c99aa8cf484b53f
ijensen <jenseniain@gmail.com>
2022-08-28 13:20:46 +1000

    Mobile Support

--- static/js/script.js
+++ static/js/script.js
@@ -240,8 +240,6 @@ function hideEnd(){
 
 function startup() {
   canvas.addEventListener('touchstart', handleStart);
-  canvas.addEventListener('touchend', handleEnd);
-  canvas.addEventListener('touchcancel', handleCancel);
   canvas.addEventListener('touchmove', handleMove);
 }
 
@@ -251,16 +249,12 @@ const ongoingTouches = [];
 
 function handleStart(evt) {
   evt.preventDefault();
-  log('touchstart.');
-  const el = document.getElementById('canvas');
-  const ctx = el.getContext('2d');
+  console.log('touchstart.');
   const touches = evt.changedTouches;
 
   for (let i = 0; i < touches.length; i++) {
-    log(`touchstart: ${i}.`);
     ongoingTouches.push(copyTouch(touches[i]));
     const color = colorForTouch(touches[i]);
-    log(`color of touch with id ${touches[i].identifier} = ${color}`);
     ctx.beginPath();
     ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
     ctx.fillStyle = color;
@@ -268,97 +262,51 @@ function handleStart(evt) {
   }
 }
 
-// function handleMove(evt) {
-//   evt.preventDefault();
-//   const el = document.getElementById('canvas');
-//   const ctx = el.getContext('2d');
-//   const touches = evt.changedTouches;
+function handleMove(evt) {
+  evt.preventDefault();
+  const touches = evt.changedTouches;
 
-//   for (let i = 0; i < touches.length; i++) {
-//     const color = colorForTouch(touches[i]);
-//     const idx = ongoingTouchIndexById(touches[i].identifier);
+  for (let i = 0; i < touches.length; i++) {
+    const idx = ongoingTouchIndexById(touches[i].identifier);
 
-//     if (idx >= 0) {
-//       log(`continuing touch ${idx}`);
-//       ctx.beginPath();
-//       log(`ctx.moveTo( ${ongoingTouches[idx].pageX}, ${ongoingTouches[idx].pageY} );`);
-//       ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-//       log(`ctx.lineTo( ${touches[i].pageX}, ${touches[i].pageY} );`);
-//       ctx.lineTo(touches[i].pageX, touches[i].pageY);
-//       ctx.lineWidth = 4;
-//       ctx.strokeStyle = color;
-//       ctx.stroke();
+    if (idx >= 0) {
+      ctx.beginPath();
+      ctx.moveTo(ongoingTouches[idx].pageX-$('#canvasView').offset().left, ongoingTouches[idx].pageY-$('#canvasView').offset().top);
+      ctx.lineTo(touches[i].pageX-$('#canvasView').offset().left, touches[i].pageY-$('#canvasView').offset().top);
+      ctx.lineWidth = lineWid;
+      ctx.strokeStyle = col;
+      ctx.stroke();
 
-//       ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
-//     } else {
-//       log('can\'t figure out which touch to continue');
-//     }
-//   }
-// }
+      ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
+    } else {
+      console.log(log('can\'t figure out which touch to continue'));
+    }
+  }
+}
 
-// function handleEnd(evt) {
-//   evt.preventDefault();
-//   log("touchend");
-//   const el = document.getElementById('canvas');
-//   const ctx = el.getContext('2d');
-//   const touches = evt.changedTouches;
+function colorForTouch(touch) {
+  let r = touch.identifier % 16;
+  let g = Math.floor(touch.identifier / 3) % 16;
+  let b = Math.floor(touch.identifier / 7) % 16;
+  r = r.toString(16); // make it a hex digit
+  g = g.toString(16); // make it a hex digit
+  b = b.toString(16); // make it a hex digit
+  const color = `#${r}${g}${b}`;
+  return color;
+}
 
-//   for (let i = 0; i < touches.length; i++) {
-//     const color = colorForTouch(touches[i]);
-//     let idx = ongoingTouchIndexById(touches[i].identifier);
+function copyTouch({ identifier, pageX, pageY }) {
+  return { identifier, pageX, pageY };
+}
 
-//     if (idx >= 0) {
-//       ctx.lineWidth = 4;
-//       ctx.fillStyle = color;
-//       ctx.beginPath();
-//       ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-//       ctx.lineTo(touches[i].pageX, touches[i].pageY);
-//       ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8);  // and a square at the end
-//       ongoingTouches.splice(idx, 1);  // remove it; we're done
-//     } else {
-//       log('can\'t figure out which touch to end');
-//     }
-//   }
-// }
+function ongoingTouchIndexById(idToFind) {
+  for (let i = 0; i < ongoingTouches.length; i++) {
+    const id = ongoingTouches[i].identifier;
 
-// function handleCancel(evt) {
-//   evt.preventDefault();
-//   log('touchcancel.');
-//   const touches = evt.changedTouches;
+    if (id === idToFind) {
+      return i;
+    }
+  }
+  return -1;    // not found
+}
 
-//   for (let i = 0; i < touches.length; i++) {
-//     let idx = ongoingTouchIndexById(touches[i].identifier);
-//     ongoingTouches.splice(idx, 1);  // remove it; we're done
-//   }
-// }
-
-// function colorForTouch(touch) {
-//   let r = touch.identifier % 16;
-//   let g = Math.floor(touch.identifier / 3) % 16;
-//   let b = Math.floor(touch.identifier / 7) % 16;
-//   r = r.toString(16); // make it a hex digit
-//   g = g.toString(16); // make it a hex digit
-//   b = b.toString(16); // make it a hex digit
-//   const color = `#${r}${g}${b}`;
-//   return color;
-// }
-
-// function copyTouch({ identifier, pageX, pageY }) {
-//   return { identifier, pageX, pageY };
-// }
-
-// function ongoingTouchIndexById(idToFind) {
-//   for (let i = 0; i < ongoingTouches.length; i++) {
-//     const id = ongoingTouches[i].identifier;
-
-//     if (id === idToFind) {
-//       return i;
-//     }
-//   }
-//   return -1;    // not found
-// }
-
-// function log(msg) {
-//   const container = document.getElementById('log');
-//   container.textContent = `${msg} \n${container.textContent}`;
-// }
